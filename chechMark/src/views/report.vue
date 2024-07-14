36c55c47edf01c4dac1e859e2e90bafc34c2e44e
mehrab-ghasemzadeh <mehrab.ghw@gmail.com>
2024-07-14 17:16:57 +0330

    nc

--- chechMark/src/views/report.vue
+++ chechMark/src/views/report.vue
@@ -1,16 +1,9 @@
 <template>
-  <!-- <vh
-    :showNavCol="showNavCol"
-    :mainInWidth="mainInWidth"
-    @showNavChangeState="showNavCol= !showNavCol; console.log(showNavCol)"
-    @mainInWidthChange="(size) => mainInWidth = size"
-  ></vh> -->
   <div id="container" class="w-full">
-    <h1>fucck</h1>
+    <h1>
+      reports page
+    </h1>
   </div>
-  <!-- <div id="nav" class="p-3 flex flex-col items-center" v-show="showNavCol">
-    <vn :loggedIn="loggedIn"></vn>
-  </div> -->
 </template>
 
 <script>
